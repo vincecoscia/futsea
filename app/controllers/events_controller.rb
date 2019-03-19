7ec8d8d7a3d33c404687c428005d56b2698cb3e9
Andy Weiss <andyweiss1982@gmail.com>
2019-03-19 14:47:09 -0400

    weird date stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,16 +2,15 @@ class EventsController < ApplicationController
 
 
   def index
-    date = params[:date] || Date.current
+    @date = Date.parse(params[:date]) rescue Date.current
     page = (params[:page] || 1).to_i
     per_page  = 5
     @location = Location.find(params[:location_id])
     @events = Location.find_by_id(params[:location_id])
                 .events
-                .where(date: date)
+                .where(date: @date)
                 .ordered
                 .paginate(page: page, per_page: per_page)
-
   end
 
   def show
