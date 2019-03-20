3a4deb689261945e7f49e97e82cad3480de91552
Tommy <tsuwunrut@carecloud.com>
2019-03-20 19:54:23 -0400

    mapbox

--- app/javascript/packs/app-bundle.js
+++ app/javascript/packs/app-bundle.js
@@ -1,10 +1,10 @@
-import ReactOnRails from 'react-on-rails';
-
-import Search from '../bundles/Search/components/Search';
-import Locations from '../bundles/Locations/components/Locations';
+import ReactOnRails from "react-on-rails";
+import Search from "../bundles/Search/components/Search";
+import Locations from "../bundles/Locations/components/Locations";
+import "babel-polyfill";
 
 // This is how react_on_rails can see the Search in the browser.
 ReactOnRails.register({
   Search,
-  Locations,
+  Locations
 });
